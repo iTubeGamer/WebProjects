74795d31eae09363bfb17e441d0affa803f999ce
Max Kroner <development@maxkroner.de>
2019-02-06 18:39:44 +0100

    visualizing marketcap

--- stock_universe/code.js
+++ stock_universe/code.js
@@ -6,6 +6,8 @@
   var tradingview;
   var Graph = ForceGraph3D();
   var graphData;
+  var marketcap_median;
+  const lie_factor = 2;
 	  
   window.onload = function start(){
 	  graph_elem = document.getElementById('3d-graph');
@@ -27,29 +29,29 @@
 	  
 	
 	const start = new Date();
-
-	session
-	.run('MATCH (n)-[r]-(m)' +
-		//` WHERE (n.sector in ['Technology', 'Healthcare', 'Energy'])` +
-		//` AND (m.sector in ['Technology', 'Healthcare', 'Energy'])` +
-		' WHERE r.correlation > 0.95' +
-		' RETURN { id: id(n), sector:n.sector, symbol:n.symbol, industry:n.industry, company:n.company } as source, { id: id(m), sector:m.sector, symbol:m.symbol, industry:m.industry, company:m.company } as target')
+	const nodes = {};
+		
+	session.run('MATCH (n)-[r]-(m)' +
+		' WHERE r.correlation > 0.97' +
+		' RETURN { id: id(n), marketcap:n.marketcap, sector:n.sector, symbol:n.symbol, industry:n.industry, company:n.company } as source, { id: id(m), marketcap:m.marketcap, sector:m.sector, symbol:m.symbol, industry:m.industry, company:m.company } as target')
 	.then(function (result) {
-	  const nodes = {}
 	  
 	  // turn records into list of link-objects
 	  const links = result.records.map(r => 
-		  { var source = r.get('source');source.id = source.id.toNumber();
-           nodes[source.id] = source;
-	       var target = r.get('target');target.id = target.id.toNumber();
-           nodes[target.id] = target;
-	       return {source:source.id,target:target.id}
+		  { var source = r.get('source');
+		   source.id = source.id.toNumber();
+		   nodes[source.id] = source;
+		   var target = r.get('target');
+		   target.id = target.id.toNumber();
+		   nodes[target.id] = target;
+		   return {source:source.id,target:target.id}
 					});
 					
 	  session.close();
 	  
 	  // convert nodes object to array
-	  const nodes_array = Object.values(nodes);  
+	  const nodes_array = Object.values(nodes);
+	  marketcap_median = calculateMarketcapMedian(nodes_array);
 	  console.log(links.length + ' links and ' + nodes_array.length + ' nodes loaded in '  + (new Date()-start) + ' ms.');
 	  
 	  // combine nodes and links to graph
@@ -57,16 +59,16 @@
 	  
 	  //draw graph
 	  Graph(graph_elem)
-	    .enableNodeDrag(false)
-        .onNodeHover(node => graph_elem.style.cursor = node ? 'pointer' : null)
-		.d3AlphaDecay(0.01)
+		.enableNodeDrag(false)
+		.onNodeHover(node => graph_elem.style.cursor = node ? 'pointer' : null)
+		.d3AlphaDecay(0.001)
 		.graphData(graphData)
 		.nodeAutoColorBy('sector')
 		.nodeLabel(node => `${node.company} (${node.sector})`)
+		.nodeRelSize(4)
+		.nodeVal(node => getVolume(node))
 		.onNodeClick(node => {
-			
-			newWidget(node.symbol);
-			//window.open(`https://iextrading.com/apps/stocks/${node.symbol}`);
+			window.open(`https://iextrading.com/apps/stocks/${node.symbol}`);
 		 });
 	 
 	})
@@ -76,14 +78,30 @@
 
   }
   
-  function onSelectColoring(){
+  function calculateMarketcapMedian(nodes){
+	  nodes.sort(function(a,b){
+		return a.marketcap-b.marketcap;
+	  });
 	  
-	  var color_by = document.getElementById("coloring").value;
-	  console.log('changing coloring to ' + color_by);
-	  Graph(graph_elem)
-		.nodeAutoColorBy(color_by)
-		.graphData(graphData); 
+	  if(nodes.length ===0) return 0
+  
+	  var half = Math.floor(nodes.length / 2);
+
+	  return nodes[half].marketcap;  
   }
+  
+
+
+function getVolume(node){
+	let volume = node.marketcap / marketcap_median;
+	
+	return 1 + ((volume - 1) / lie_factor);
+}
+  
+  
+
+  
+  
   
 
 	
